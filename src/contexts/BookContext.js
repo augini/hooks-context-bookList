ad3ca320be59b0ca94d22ef6284f3803dde457c2
Farrukh Atabekov <augini18@gmail.com>
2020-06-29 05:16:05 +0900

    --update: add deploy scripts

--- src/contexts/BookContext.js
+++ src/contexts/BookContext.js
@@ -1,26 +1,18 @@
-import React, {createContext, useReducer} from 'react';
+import React, {createContext, useReducer, useEffect} from 'react';
 import {bookReducer} from '../reducers/bookReducer'
 
 export const BookContext = createContext()
 
 const BookContextProvider = (props) => {
-  // const [books, setBooks] = useState([
-  //   {title: 'Rich Dad, Poor Dad', author: 'Robert Kiyosaki', id: 1 },
-  //   {title: 'Yulduzli Tunlar', author: 'Pirimqul Qodirov', id: 2 }
-  // ])
 
-  const [books, dispatch] = useReducer(bookReducer, [
-    {title: 'Rich Dad, Poor Dad', author: 'Robert Kiyosaki', id: 1 },
-    {title: 'Yulduzli Tunlar', author: 'Pirimqul Qodirov', id: 2 }
-  ])
+  const [books, dispatch] = useReducer(bookReducer, [], () => {
+    const localData = localStorage.getItem('books')
+    return localData ? JSON.parse(localData) : []
+  })
 
-  // const addBook = (title, author) => {
-  //   setBooks([...books, {title, author, id: uuidv4()}])
-  // }
-
-  // const removeBook = (id) => {
-  //   setBooks( books.filter (book => book.id !== id) )
-  // }
+  useEffect(() =>{
+    localStorage.setItem( 'books', JSON.stringify(books))
+  },[books])
 
   return ( 
     <BookContext.Provider value = {{books, dispatch}}>
